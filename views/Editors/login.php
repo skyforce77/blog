bf919d63c540dc3968af8df83e7a25b245fed1ce
Skyforce787 <arnaud.bruyer@gmail.com>
2015-05-09 19:16:33 +0200

    login

--- views/Editors/login.php
+++ views/Editors/login.php
@@ -1,22 +1,16 @@
 <br>
 <br>
-<div class="container page-content">
-	<div class="panel">
-		<div class="heading">
-			<span class="title">Connexion</span>
-		</div>
-		<div class="content">
-			 <form action="#" method="POST">
-				Nom d'utilisateur:<br>
-				<input type="text" name="login">
-				<br>
-				Mot de passe:<br>
-				<input type="password" name="passwd">
-				<br>
-				<input type="submit" value="Se connecter">
-			</form> 
-		</div>
-	</div>
+<div class="container page-content bg-teal">
+	 <form action="#" method="POST">
+		<h1 class="text-light">Login to service</h1>
+		Nom d'utilisateur:<br>
+		<input type="text" name="login">
+		<br>
+		Mot de passe:<br>
+		<input type="password" name="passwd">
+		<br>
+		<input type="submit" value="Se connecter">
+	</form> 
 </div>
 <br>
 <br>
